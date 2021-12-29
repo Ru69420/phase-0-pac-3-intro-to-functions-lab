e8b6867ba3849bc57c5a0568243e2b617b209271
RuthTadesse <tib6297@gmail.com>
2021-12-29 18:40:18 -0500

    Completed assignment

---
+++ index.js
@@ -0,0 +1,68 @@
+function shout(string) {
+    // todo
+  }
+  function shout(string) {
+    return string;
+  }
+  "Hello!".toUpperCase(); // 'HELLO!'
+  function shout(string) {
+    return string.toUpperCase();
+  }
+
+
+
+  function whisper(string) {
+    // todo
+  }
+  function whisper(string) {
+    return string;
+  }
+  "Hello!".toLowerCase(); // 'hello!'
+  function whisper(string) {
+    return string.toLowerCase();
+  }
+
+
+
+  function logShout(string) {
+    // todo
+  }
+  function logShout(string) {
+    return string;
+  }
+  console.log("HELLO")
+  const string = "Hello!";
+
+console.log(string);
+console.log(string.toUpperCase());
+
+string === string.toUpperCase();
+  function shout(string) {
+    return string.toUpperCase();
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  function sayHiToHeadphonedRoommate(string) {
+    // todo
+  }
+  function sayHiToHeadphonedRoommate(string) {
+    return string.toUppercase("YES INDEED!");
+  }
+  "Hello!".toUpperCase(); // 'YES INDEED!'
+  function sayHiToHeadphonedRoommate(string) {
+    return string.toUpperCase();
+  }
+
